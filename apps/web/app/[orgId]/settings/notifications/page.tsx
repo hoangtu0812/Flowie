972277d9e757a89428f1d06e9d1e8ecb00489d91
tuph-laptop <tuph@bsr.com.vn>
2026-08-22 15:19:30 +0700

    feat: use Discord for workspace notification settings

--- apps/web/app/[orgId]/settings/notifications/page.tsx
+++ apps/web/app/[orgId]/settings/notifications/page.tsx
@@ -1,11 +1,11 @@
 import MainLayout from '@/components/layout/main-layout';
-import AccountNotifications from '@/components/common/settings/account-notifications';
+import { DiscordIntegration } from '@/components/settings/discord-integration';
 import Header from '@/components/layout/headers/settings/header';
 
 export default function Page() {
    return (
       <MainLayout header={<Header />} headersNumber={1}>
-         <AccountNotifications />
+         <DiscordIntegration />
       </MainLayout>
    );
 }
